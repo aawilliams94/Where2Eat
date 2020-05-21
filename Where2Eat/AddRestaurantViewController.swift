db0173d3f1b38db7193e625438ea5ba9fdf4eb1f
Ashanti Williams <ashanti.a.williams@hotmail.com>
2020-05-21 16:55:39 -0500

    End of meeting 1

---
+++ Where2Eat/AddRestaurantViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AddRestaurantViewController.swift
+//  Where2Eat
+//
+//  Created by Ashanti Williams on 5/21/20.
+//  Copyright © 2020 Ashanti Williams. All rights reserved.
+//
+
+import UIKit
+
+class AddRestaurantViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var nameOfRestaurant: UITextField!
+        
+        
+
+    @IBAction func saveRestaurantButton(_ sender: UIButton) {
+    }
+    
+    
+
+}
