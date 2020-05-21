8f5a30a905c6046e4b72e6bbc3158f89e63f4bd9
Ashanti Williams <ashanti.a.williams@hotmail.com>
2020-05-21 16:49:00 -0500

    Added the functionality for the random button to give an alert.

---
+++ Where2Eat/Where2EatViewController.swift
@@ -0,0 +1,36 @@
+//
+//  Where2EatViewController.swift
+//  Where2Eat
+//
+//  Created by Ashanti Williams on 5/21/20.
+//  Copyright © 2020 Ashanti Williams. All rights reserved.
+//
+
+import UIKit
+
+class Where2EatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+    @IBAction func randomButton(_ sender: UIButton) {
+    /*
+        let selectedEmotion = sender.titleLabel!.text
+        
+        let messageOptions = songOptions[emojis[selectedEmotion!]!]!.count
+        let emojiMessage = songOptions[emojis[selectedEmotion!]!]?[Int.random(in:0 ... messageOptions-1)]
+         
+        //create the alert
+        let emojiAlert =  UIAlertController (title: "Here's a restaurant!", message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
+        
+    }
+ 
+ */
+
+    }
+}
