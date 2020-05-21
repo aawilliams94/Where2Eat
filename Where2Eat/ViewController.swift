1dfdfdda03f1bc1bf369d32a788c41ad69321350
Ashanti Williams <ashanti.a.williams@hotmail.com>
2020-05-21 15:04:45 -0500

    Initial Commit

---
+++ Where2Eat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Where2Eat
+//
+//  Created by Ashanti Williams on 5/21/20.
+//  Copyright © 2020 Ashanti Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
